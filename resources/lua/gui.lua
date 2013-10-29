014078f9102c43bc0c2af129455ff0eac9fe0875
Julzso23 <julzso23@gmail.com>
2013-10-29 08:54:00 -0700

    Custom music from players!

--- resources/lua/gui.lua
+++ resources/lua/gui.lua
@@ -1,19 +1,21 @@
 jpm.gui = {}
 jpm.gui.__index = jpm.gui
 
-function jpm.gui.newButton(x, y, w, h)
+
+function jpm.gui.newButton(text, x, y, w, h)
 	local g = {}
 	setmetatable(g, jpm.gui)
 
-	g.x = jpm.screen.x(x)
-	g.y = jpm.screen.y(y)
-	g.w = jpm.screen.x(w)
-	g.h = jpm.screen.x(h)
+	g.text = text
+	g.x = x
+	g.y = y
+	g.w = w
+	g.h = h
 
 	return g
 end
 
-function jpm.gui:onClick(x, y, button)
+function jpm.gui:onClicked(x, y, button)
 	if button == "l" then
 	end
 end
--- resources/lua/music.lua
+++ resources/lua/music.lua
@@ -1,13 +1,37 @@
 jpm.music = {}
 
 function jpm.music.init()
-	jpm.music.current = love.audio.newSource("resources/audio/"..math.random(1,5)..".mp3", "stream")
-	jpm.music.current:play()
+	if not love.filesystem.exists("music") then
+		love.filesystem.mkdir("music")
+	end
+	jpm.music.userFiles = love.filesystem.enumerate("music")
+	jpm.music.userMusic = {}
+	for k, v in pairs(jpm.music.userFiles) do
+		if love.filesystem.isFile("music/"..v) then
+			table.insert(jpm.music.userMusic, "music/"..v)
+		end
+	end
+	
+	if #jpm.music.userMusic > 0 then
+		jpm.music.currentId = 1
+		jpm.music.current = love.audio.newSource(jpm.music.userMusic[jpm.music.currentId], "stream")
+		jpm.music.current:play()
+	else
+		jpm.music.current = love.audio.newSource("resources/audio/"..math.random(1,5)..".mp3", "stream")
+		jpm.music.current:play()
+	end
 end
 
 function jpm.music.update()
 	if jpm.music.current:isStopped() then
 		jpm.music.current = love.audio.newSource("resources/audio/"..math.random(1,5)..".mp3", "stream")
+		if #jpm.music.userMusic > jpm.music.currentId then
+			jpm.music.currentId = jpm.music.currentId + 1
+			jpm.music.current = love.audio.newSource(jpm.music.userMusic[jpm.music.currentId], "stream")
+		else
+			jpm.music.currentId = 1
+			jpm.music.current = love.audio.newSource(jpm.music.userMusic[jpm.music.currentId], "stream")
+		end
 		jpm.music.current:play()
 	end
 end
